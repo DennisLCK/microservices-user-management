7b46c556d2446dad7e8c0f67dcec04b4db3a5aba
dennis_liu <dennis_liu@gss.com.tw>
2021-11-25 03:32:15 +0800

    Adding axon config and mongodb config.

--- user.command.api/src/main/java/com/dennislck/user/command/api/UserCommandApplication.java
+++ user.command.api/src/main/java/com/dennislck/user/command/api/UserCommandApplication.java
@@ -1,9 +1,12 @@
 package com.dennislck.user.command.api;
 
+import com.dennislck.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCommandApplication {
 
 	public static void main(String[] args) {
--- user.query.api/src/main/java/com/dennislck/user/query/api/UserQueryApplication.java
+++ user.query.api/src/main/java/com/dennislck/user/query/api/UserQueryApplication.java
@@ -1,9 +1,12 @@
 package com.dennislck.user.query.api;
 
+import com.dennislck.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserQueryApplication {
 
 	public static void main(String[] args) {
