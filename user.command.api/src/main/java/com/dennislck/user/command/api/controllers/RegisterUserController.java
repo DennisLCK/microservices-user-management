5b41bf2098a9c4671cc3a42869ad8bddd8608287
dennis_liu <dennis_liu@gss.com.tw>
2021-11-28 04:01:47 +0800

    Adding first controller - RegisterUserController and entity validation and dto.

---
+++ user.command.api/src/main/java/com/dennislck/user/command/api/controllers/RegisterUserController.java
@@ -0,0 +1,43 @@
+package com.dennislck.user.command.api.controllers;
+
+import com.dennislck.user.command.api.commands.RegisterUserCommand;
+import com.dennislck.user.command.api.dto.RegisterUserResponse;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/api/v1/registerUser")
+public class RegisterUserController {
+	private final CommandGateway commandGateway;
+
+	@Autowired
+	public RegisterUserController(CommandGateway commandGateway) {
+		this.commandGateway = commandGateway;
+	}
+
+	@PostMapping
+	public ResponseEntity<RegisterUserResponse> registerUser(@RequestBody RegisterUserCommand command) {
+		command.setId(UUID.randomUUID().toString());
+
+		try {
+			commandGateway.sendAndWait(command);
+			String successMessage = "User successfully registered!";
+
+			return new ResponseEntity<>(new RegisterUserResponse(successMessage), HttpStatus.CREATED);
+		} catch (Exception e) {
+			String safeErrorMessage = "Error while processing register user request for id - " + command.getId();
+			System.out.println(e.getMessage());
+
+			return new ResponseEntity<>(new RegisterUserResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
---
+++ user.command.api/src/main/java/com/dennislck/user/command/api/dto/BaseResponse.java
@@ -0,0 +1,10 @@
+package com.dennislck.user.command.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BaseResponse {
+	String message;
+}
